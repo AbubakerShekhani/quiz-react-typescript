8f2209d8c276c22c30cfd22f8de40470f239a653
Abubaker  Shekhani <abubakershekhani@gmail.com>
2020-08-14 00:47:35 +0500

    Update firebase-messaging-sw.js

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -44,12 +44,23 @@ self.addEventListener('install', (event) => {
 })
 
 self.addEventListener('fetch', event => {
+  console.log('Fetch event for ', event.request.url);
   event.respondWith(
-    fetch(event.request).then(response => {
-      cache.put(event.request, response.clone());
-      return response;
-    }).catch(_ => {
-      return caches.match(event.request);
+    caches.match(event.request)
+    .then(response => {
+      if (response) {
+        console.log('Found ', event.request.url, ' in cache');
+        return response;
+      }
+      console.log('Network request for ', event.request.url);
+      return fetch(event.request)
+
+      // TODO 4 - Add fetched files to the cache
+
+    }).catch(error => {
+
+      // TODO 6 - Respond with custom offline page
+
     })
-  )
- });
+  );
+});
