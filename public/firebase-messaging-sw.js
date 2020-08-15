622ea6b86fe4d13e188588eb9a9b5cbdb7aae143
Abubaker  Shekhani <abubakershekhani@gmail.com>
2020-08-15 14:26:50 +0500

    Create custom service worker for PWA

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -30,39 +30,3 @@ self.addEventListener('notificationclick', event => {
   return event;
 });
 
-const dynamicContentCache = 'QuizApp-Dynamic-Content';
-const cacheAssets = [
-    'https://opentdb.com/api.php?amount=10&category=18&type=multiple&difficulty=easy',
-];
-
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(dynamicContentCache).then((cache)=>{
-          console.log("cache");
-          cache.addAll(cacheAssets);
-
-        })
-    )
-})
-
-self.addEventListener('fetch', event => {
-  console.log('Fetch event for ', event.request.url);
-  event.respondWith(
-    caches.match(event.request)
-    .then(response => {
-      if (response) {
-        console.log('Found ', event.request.url, ' in cache');
-        return response;
-      }
-      console.log('Network request for ', event.request.url);
-      return fetch(event.request)
-
-      // TODO 4 - Add fetched files to the cache
-
-    }).catch(error => {
-
-      // TODO 6 - Respond with custom offline page
-
-    })
-  );
-});
---
+++ public/sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = "static";
+const urlsToCache = [
+  '/',
+  'index.html',
+  '/favicon.ico',
+                      '/static/css/2.ed7b5974.chunk.css',
+                      '/static/css/main.16e04ad9.chunk.css',
+                    '/static/js/2.19b408b1.chunk.js',
+                    '/static/js/main.7be08d8d.chunk.js',
+                    '/static/js/runtime-main.15334abf.js',
+                    'https://opentdb.com/api.php?amount=10&category=18&type=multiple&difficulty=easy'];
+
+const self = this;
+
+// Install SW
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                console.log('Opened cache');
+
+                return cache.addAll(urlsToCache);
+            })
+    )
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
